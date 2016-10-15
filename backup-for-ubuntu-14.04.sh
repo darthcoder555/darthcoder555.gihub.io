167370f8fae0b0b9462f49976145d456d9d88bcc
Prasath Rajan <prasathindiarajan@gmail.com>
2016-10-15 19:06:07 +0530

    added backup-for-ubuntu-14.04.sh

---
+++ backup-for-ubuntu-14.04.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get install vlc vlc-plugin-*
+
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+sudo add-apt-repository ppa:xorg-edgers/ppa -y
+sudo apt-get update
+sudo apt-get install nvidia-current
+
+sudo apt-get -y install r-base
